ed9d1870a39fed7270f745750b84324424589368
Seth Rhodes <seth@thinkpixbit.com>
2015-09-13 11:36:56 -0400

    Gulpfile: vet & sass tasks, also default task

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,46 @@
 
   var del = require('del');
   var gulp = require('gulp');
+  var jshint = require('gulp-jshint');
+  var jscs = require('gulp-jscs');
+  var runSequence = require('run-sequence');
+  var sass = require('gulp-sass');
+  var stylish = require('gulp-jscs-stylish');
+
   var config = require('./config.js');
+
   gulp.task('ci', function ( done ) {
     return runTests( true, done );
   });
 
   gulp.task('clean', function(){
     return del(['build']);
+  });
+
+  gulp.task('vet', function(){
+    return gulp
+      .src( config.allJS )
+      .pipe( jshint() )
+      .pipe( jscs() )
+      .on( 'error', function(){} )
+      .pipe( stylish.combineWithHintResults() )
+      .pipe( jshint.reporter('jshint-stylish') );
+  });
+
+  gulp.task('sass', function(){
+    return gulp
+    .src( config.mainSass )
+    .pipe( sass() )
+    .pipe( gulp.dest( config.buildFolder ) );
+  });
+
+  gulp.task('default', function ( done ) {
+    runSequence(
+      'clean',
+      'vet',
+      'sass',
+      done
+    );
   });
 
   function runTests( singleRun, done ) {
