b5f86f1a0e4a5f888772109aae493c897b39c21b
Seth Rhodes <seth@thinkpixbit.com>
2015-09-13 11:16:57 -0400

    Gulpfile: starter gulpfile, will expand very soon

---
+++ Gulpfile.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  var del = require('del');
+  var gulp = require('gulp');
+  
+  gulp.task('ci', function ( done ) {
+    return runTests( true, done );
+  });
+
+  gulp.task('clean', function(){
+    return del(['build']);
+  });
+
+  function runTests( singleRun, done ) {
+    var karma = require('karma').server;
+
+    karma
+      .start(
+        {
+          configFile: __dirname + '/karma.conf.js',
+          exclude: [],
+          singleRun: singleRun
+        },
+        karmaCompleted
+      );
+
+    function karmaCompleted ( karmaResult ) {
+      //karmaResult will be 1 if error
+      return karmaResult === 1 ?
+      done('Tests failed') :
+      done();
+    }
+  }
+
+
+
+}());
