605399278fd37924af0ddb6c9b4fcd4b7e158d96
Seth Rhodes <seth@thinkpixbit.com>
2015-09-14 10:11:05 -0400

    config: more variables for build/client definitions

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
+var src = 'src/';
+var client = 'build/';
 module.exports = {
   allJS: [
     './Gulpfile.js',
-    'src/**/*.js'
+    src + '**/*.js'
   ],
   //TODO: define this
-  mainSass: ''
+  mainSass: '',
+  index: src + 'index.html',
+  buildComponents: [
+    src + '**/*.module.js',
+    src + '**/*',
+    '!' + src + '**/*.spec.js'
+  ],
+  client: client,
+  clientIndex: client + 'index.html',
+  customJS: client + '**/*.js'
 };
