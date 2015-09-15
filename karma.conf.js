580f7a058cc851bf3f5f375fc7908fa175bc1302
Seth Rhodes <seth@thinkpixbit.com>
2015-09-15 09:46:16 -0400

    tests: moved unit test into related folder per angular-styleguide, changed script loading in karma

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'src/**/*.module.js',
       'src/**/*.js',
-      'test/unit/**/*'
+      'src/**/*.spec.js'
     ],
 
 
