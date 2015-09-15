f99fb34a945d763598cbca4ff64e4078e3e2928c
Seth Rhodes <seth@thinkpixbit.com>
2015-09-15 11:19:00 -0400

    protractor.conf: using jasmine-spec-reporter as reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,5 +3,15 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
     'test/e2e/**/*.js'
-  ]
+  ],
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+  },
+  browsers: [
+    'Chrome'
+  ],
+  jasmineNodeOpts: {
+    print: function(){}
+  }
 };
