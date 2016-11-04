db9f49648dfafe90fca47eea750ecc9d30464c32
Seth Rhodes <seth@thinkpixbit.com>
2016-11-04 00:50:38 -0400

    dot files for eslint and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "google",
+  "env": {
+    "browser": true
+  },
+  "globals": {
+    "angular": true
+  },
+  "rules": {
+    "require-jsdoc": 0,
+    "new-cap": 0,
+    "padded-blocks": 1
+  }
+};
