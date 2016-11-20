f08660db2aa01359a97699797937c0931f289010
Seth Rhodes <seth@thinkpixbit.com>
2016-11-20 16:50:45 -0500

    app.module: imports, strip iife

--- src/app.module.js
+++ src/app.module.js
@@ -1,37 +1,26 @@
-(function () {
-  'use strict';
+import angular from 'angular';
+import uirouter from 'angular-ui-router';
+import './stylesheets/app.scss';
 
-  /* globals angular */
+let appDependencies = [
+  uirouter
+];
 
-  var PixbitAppSeedDependencies = [
-    'ui.router',
-    'feature1',
-    'feature2'
-  ];
+angular
+  .module('mainApp', appDependencies)
+  .config(config);
 
-  angular
-    .module('PixbitAppSeed', PixbitAppSeedDependencies)
-    .config(config);
+function config($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/login');
 
-  config.$inject = [
-    '$stateProvider',
-    '$urlRouterProvider'
-  ];
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'feature1/feature1.html',
+      controller: 'LoginController',
+      controllerAs: 'LoginCtrl'
+    });
+}
 
-  function config($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/state1');
-
-    $stateProvider
-      .state('state1', {
-        url: '/state1',
-        templateUrl: 'feature1/feature1.html',
-        controller: 'FeatureOneController'
-      })
-      .state('state2', {
-        url: '/state2',
-        templateUrl: 'feature2/feature2.html',
-        controller: 'FeatureTwoController'
-      });
-  }
-
-}());
+let mainAppModule = angular.module('mainApp').name;
+export default mainAppModule;
