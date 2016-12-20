765ca0efb1665839e70c18124f0c6a97a682de13
Seth Rhodes <seth@thinkpixbit.com>
2016-12-20 17:18:50 -0500

    feature2.spec: formatting

--- src/feature2/feature2.spec.js
+++ src/feature2/feature2.spec.js
@@ -1,29 +1,29 @@
-(function () {
+(function() {
   'use strict';
   /* globals beforeEach,inject,describe,it,expect */
 
-  describe('feature2', function () {
+  describe('feature2', function() {
     beforeEach(module('feature2'));
 
     var $controller;
 
-    beforeEach(inject(function (_$controller_) {
+    beforeEach(inject(function(_$controller_) {
       $controller = _$controller_;
     }));
 
-    describe('FeatureTwoController', function () {
+    describe('FeatureTwoController', function() {
       var $scope = null;
       var FeatureTwoController = null;
-      beforeEach(function () {
+      beforeEach(function() {
         $scope = {};
         FeatureTwoController = $controller('FeatureTwoController', { $scope: $scope });
       });
 
-      it('should exist', function () {
+      it('should exist', function() {
         expect(typeof FeatureTwoController).to.equal('object');
       });
 
-      it('should have a greeting', function () {
+      it('should have a greeting', function() {
         expect($scope.greeting).to.equal('FeatureTwoController');
       });
 
